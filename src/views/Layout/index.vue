3653d9ee580b3d7f8fa6805eed1c64a0fecaee4c
bigsummer66 <2537753017@qq.com>
2026-06-24 23:27:34 +0800

    吸顶导航交互

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,10 +2,12 @@
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
 <template>
-    <LayoutHeader />
+    <LayoutFixed />
     <LayoutNav />
+    <LayoutHeader />
     <RouterView />
     <LayoutFooter />
 </template>
