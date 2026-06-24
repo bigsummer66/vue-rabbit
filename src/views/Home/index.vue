bd580fcf6031e84bcee8d00412c0e94b5cafabdd
bigsummer66 <2537753017@qq.com>
2026-06-25 01:41:55 +0800

    Home-面板组件封装props插槽

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,17 @@ import homeProduct from './components/HomeProduct.vue'
     </div>
     <HomeNew />
     <HomeHot />
-    <homeProduct />
+    <HomeProduct />
+
+
+    <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+        <div>
+            我是新鲜好物
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subTitle="热销商品 精选好物">
+        <div>
+            我是人气推荐
+        </div>
+    </HomePanel>
 </template>
