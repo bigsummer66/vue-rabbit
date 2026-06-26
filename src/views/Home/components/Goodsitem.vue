7d2fb6e21e350adfe0b8f6f3254246f75cb91c97
bigsummer66 <2537753017@qq.com>
2026-06-27 01:55:13 +0800

    Home-goodsitem组件封装

---
+++ src/views/Home/components/Goodsitem.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+    good: {
+        type: Object,
+        required: true,
+        default: () => ({})
+    }
+})
+</script>
+
+<template>
+    <RouterLink to="/" class="goods-item">
+        <img v-img-lazy="good.picture" alt="" />
+        <p class="name ellipsis">{{ good.name }}</p>
+        <p class="desc ellipsis">{{ good.desc }}</p>
+        <p class="price">&yen;{{ good.price }}</p>
+    </RouterLink>
+</template>
+
+
+<style lang='scss' scoped>
+.goods-item {
+    display: block;
+    width: 220px;
+    padding: 20px 30px;
+    text-align: center;
+    transition: all .5s;
+
+    &:hover {
+        transform: translate3d(0, -3px, 0);
+        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+}
+</style>
--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home.js'
 import { ref, onMounted } from 'vue'
+import goodItem from './Goodsitem.vue'
 const goodsProduct = ref([])
 
 const getGoods = async () => {
@@ -26,12 +27,7 @@ onMounted(() => {
                 </RouterLink>
                 <ul class="goods-list">
                     <li v-for="good in cate.goods" :key="good.id">
-                        <RouterLink to="/" class="goods-item">
-                            <img v-img-lazy="good.picture" alt="" />
-                            <p class="name ellipsis">{{ good.name }}</p>
-                            <p class="desc ellipsis">{{ good.desc }}</p>
-                            <p class="price">&yen;{{ good.price }}</p>
-                        </RouterLink>
+                        <good-item :good="good" />
                     </li>
                 </ul>
             </div>
