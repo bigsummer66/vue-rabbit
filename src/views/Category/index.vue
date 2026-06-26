59a0ee52b742f9b7a5335ef139d66c5c1f3de5aa
bigsummer66 <2537753017@qq.com>
2026-06-27 02:54:03 +0800

    Home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,17 @@
 import { ref, onMounted } from 'vue'
 import { getCategoryAPI } from '@/apis/category'
 import { useRoute } from 'vue-router'
+import { getHomeBannerAPI } from '@/apis/home'
+const bannerList = ref([])
+const getHomeBanner = async () => {
+    const res = await getHomeBannerAPI({
+        distributionSite: '2'
+    })
+    bannerList.value = res.result
+}
+onMounted(() => {
+    getHomeBanner()
+})
 const categoryList = ref([])
 const route = useRoute()
 onMounted(async () => {
@@ -22,10 +33,30 @@ onMounted(async () => {
             </div>
         </div>
     </div>
+    <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item.id">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    margin: 0 auto;
+    left: 0;
+    top: 0;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
+
 .top-category {
     h3 {
         font-size: 28px;
