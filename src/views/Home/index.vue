5a4b49df6e0f71326c5bf3f731f505142c5f7e5b
bigsummer66 <2537753017@qq.com>
2026-06-27 01:46:21 +0800

    Home-Product区域渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -15,16 +15,4 @@ import HomePanel from './components/HomePanel.vue'
     <HomeNew />
     <HomeHot />
     <HomeProduct />
-
-
-    <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
-        <div>
-            我是新鲜好物
-        </div>
-    </HomePanel>
-    <HomePanel title="人气推荐" subTitle="热销商品 精选好物">
-        <div>
-            我是人气推荐
-        </div>
-    </HomePanel>
 </template>
