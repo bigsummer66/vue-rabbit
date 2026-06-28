9d8730e15162c1486d5d5f977300db7547009e3e
bigsummer66 <2537753017@qq.com>
2026-06-29 00:50:46 +0800

    详情页-热榜区域基础

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,7 +2,7 @@
 import { getDetail } from '@/apis/detail'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-
+import DetailHot from './components/DetailHot.vue'
 const route = useRoute()
 const goods = ref({})
 
@@ -121,7 +121,8 @@ onMounted(() => {
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <DetailHot />
+                            <DetailHot />
                         </div>
                     </div>
                 </div>
