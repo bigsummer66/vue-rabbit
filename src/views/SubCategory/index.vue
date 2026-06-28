774594c29aef98ec916b62561aca3b9926d82c27
bigsummer66 <2537753017@qq.com>
2026-06-28 23:06:43 +0800

    二级分类-列表无线加载（老版待修改）

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -35,6 +35,19 @@ const tabChange = () => {
     reqData.value.page = 1
     getGoodList()
 }
+
+//加载更多
+const disabled = ref(false)
+const load = async () => {
+    //获取下一页数据
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodList.value = [...goodList.value, ...res.result.items]
+    //停止监听
+    if (res.result.items === 0) {
+        disabled.value = true
+    }
+}
 </script>
 
 <template>
@@ -44,7 +57,7 @@ const tabChange = () => {
             <el-breadcrumb separator=">">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                 <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName
-                    }}</el-breadcrumb-item>
+                }}</el-breadcrumb-item>
                 <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
@@ -54,7 +67,7 @@ const tabChange = () => {
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
                 <GoodItem v-for="good in goodList" :good="good" :key="good.id" />
             </div>
