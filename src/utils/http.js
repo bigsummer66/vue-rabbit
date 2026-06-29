219ff6b55ed95ea95580dca18e444a0d9c8c1592
bigsummer66 <2537753017@qq.com>
2026-06-29 23:18:14 +0800

    登录页-token失效401状态码拦截

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
-
+import router from '@/router'
 //设置请求头
 const httpInstance = axios.create({
     baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -27,11 +27,23 @@ httpInstance.interceptors.request.use(config => {
 
 //响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     //统一错误提示
     ElMessage({
         type: 'error',
         message: e.response.data.message
     })
+    //401token失效处理
+
+    //1.清除本地用户数据
+
+    //2.跳转到登录页面
+    if (e.response.status === 401) {
+        //1.清除本地用户数据
+        userStore.clearUserInfo()
+        //2.跳转到登录页面
+        router.replace({ path: '/login' }) //跳转到登录页面
+    }
     //统一错误处理
     return Promise.reject(e)
 })
