94bab62b22de898d62801d7a733ff51c7e6f8657
bigsummer66 <2537753017@qq.com>
2026-06-29 21:10:12 +0800

    登录页-勾选项自定义校验逻辑

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,7 +5,8 @@ import { ref } from 'vue'
 //1.准备表单对象
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 
 //2.准备校验规则
@@ -16,6 +17,18 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 14, message: '密码长度在6到14位之间', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                //自定义校验逻辑
+                if (value) {
+                    callback()
+                } else {
+                    callback(new Error('请同意隐私条款'))
+                }
+            },
+        }
     ]
 }
 
@@ -53,8 +66,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox v-model="form.agree" size="large">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
