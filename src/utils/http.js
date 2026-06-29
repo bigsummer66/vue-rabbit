f19cf413a63765357b50a2ecaf6e17f007cb9631
bigsummer66 <2537753017@qq.com>
2026-06-29 22:49:17 +0800

    登录页-请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,16 +2,29 @@
 import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user'
 
-
+//设置请求头
 const httpInstance = axios.create({
     baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000 // 请求超时时间
 })
 //请求拦截器
 httpInstance.interceptors.request.use(config => {
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
-}, e => Promise.reject(e))
+}, e => {
+    return Promise.reject(e)
+})
+
+
+
+
+
 //响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
     //统一错误提示
