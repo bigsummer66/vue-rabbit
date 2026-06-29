86b5538552b6b3f0f9a389ec7e8ab9ba6954fae5
bigsummer66 <2537753017@qq.com>
2026-06-30 00:46:34 +0800

    头部购物车-本地购物车列表基础渲染

--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -34,7 +34,7 @@ const cartStore = useCartStore() // 获取购物车store实例
                     <p>{{ cartStore.totalNum }} 件商品</p>
                     <p>&yen;{{ cartStore.totalPrice.toFixed(2) }} </p>
                 </div>
-                <el-button size="large" type="primary">去购物车结算</el-button>
+                <el-button size="large" type="primary" @click="$router.push('/cartlist')">去购物车结算</el-button>
             </div>
         </div>
     </div>
