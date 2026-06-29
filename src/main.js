4070db17f155fdf1eff5ea79fddd3d9598227292
bigsummer66 <2537753017@qq.com>
2026-06-29 22:32:30 +0800

    登录页-Pinia本地持久化(persistedstate插件)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components/index.js'
 
@@ -10,8 +10,11 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate) // 使用持久化插件
 
-app.use(createPinia())
+
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
