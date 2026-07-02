0d04f2358bf7009751069ab2a371b9751ce7a2fa
bigsummer66 <2537753017@qq.com>
2026-07-03 01:55:28 +0800

    会员中心-整体梳理和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -56,6 +59,23 @@ const router = createRouter({
           path: '/paycallback',
           name: 'payback',
           component: PayBack
+        },
+        {
+          path: '/member',
+          name: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              name: 'member-info',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              name: 'member-order',
+              component: UserOrder
+            }
+          ]
         }
       ]
     },
