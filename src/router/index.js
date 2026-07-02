901949240309b1eee068d8a3fca701f81b894ec3
bigsummer66 <2537753017@qq.com>
2026-07-03 01:14:49 +0800

    支付-支付结果展示

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -50,6 +51,11 @@ const router = createRouter({
           path: '/pay',
           name: 'pay',
           component: Pay
+        },
+        {
+          path: '/paycallback',
+          name: 'payback',
+          component: PayBack
         }
       ]
     },
